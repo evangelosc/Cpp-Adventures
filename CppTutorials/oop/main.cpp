8aeff769abf1579668983f63344b7099b049b4bc
evangelosc <evan.charalampidis@gmail.com>
2020-07-24 17:24:23 +0100

    24/07/2020 initial object

--- CppTutorials/oop/main.cpp
+++ CppTutorials/oop/main.cpp
@@ -9,6 +9,16 @@ struct User{
         std::string status;
 };
 
+class Userakos{
+    public:
+        std::string first;
+        std::string last;
+        std::string getStatus() { return status; }
+    private:
+        std::string status;
+};
+
+
 int main(){
     User me;
     me.first = "Nick";
@@ -16,5 +26,11 @@ int main(){
     me.getStatus() = "Gold";
 
     std::cout << "First Name: " << me.first << std::endl;
+    
+    Userakos meme;
+    meme.first = "Nickos";
+    meme.last = "Petegios";
+    meme.getStatus() = "Xrisos";
+    std::cout << "First Name: " << meme.first << std::endl;    
     return 0;
 }
